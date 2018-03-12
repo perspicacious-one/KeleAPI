64fb69de105e9af31dea2e71941835af0ccbac47
Nelson <nelsondcraig@gmail.com>
2018-03-12 15:51:16 -0700

    auth_token captured

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,33 +1,23 @@
 require "httparty"
 
-module Kele
+class Kele
 
-  class Session
-    include HTTParty
-    attr_accessor :token, :uri, :response
+  include HTTParty
+  attr_accessor :auth_token, :uri, :response
 
-    def initialize(email, pass)
-      begin
-        @uri = "https://www.bloc.io/api/v1/sessions"
-        @values = {
-          body: {
-                email: email,
-                password: pass
-          }
+  def initialize(email, pass)
+    begin
+      @uri = "https://www.bloc.io/api/v1/sessions"
+      @values = {
+        body: {
+              email: email,
+              password: pass
         }
-
-
-        @token = self.class.post(@uri, @values)
-      rescue => e
-        p e.message
-      end
-      p Module.nesting
+      }
+      @response = self.class.post(@uri, @values)
+      @auth_token = @response["auth_token"]
+    rescue => e
+      p e.message
     end
-
-    private
-
   end
-
 end
-
-# Kele::Session.new("nelsondcraig@gmail.com", "%7sCAqjH9c6a8ysB")
