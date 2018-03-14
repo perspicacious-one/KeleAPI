e679a207c80069569efb8e844eaa092526cc92f4
Nelson <nelsondcraig@gmail.com>
2018-03-14 14:44:49 -0700

    restructured code

--- lib/kele/session.rb
+++ lib/kele/session.rb
@@ -1,8 +1,11 @@
-class Session
-  include HTTParty
-  attr_reader :auth_token
 
-  def initialize(email, pass)
+module Session
+  include HTTParty
+  extend self
+
+  attr_reader :auth_token, :is_open
+
+  def start(email, pass)
     begin
       @uri = "https://www.bloc.io/api/v1/sessions"
       values = {
@@ -11,31 +14,47 @@ class Session
               password: pass
         }
       }
-      @response = self.class.post(@uri, values)
+      @response = self.post(@uri, values)
       set_token
     rescue => e
+      close
       p e.message
     end
   end
 
-  def get_user
-    user = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => @auth_token })
-    user.body
+  def get_token
+    auth_token
+  end
+
+  def open_status
+    @is_open
   end
 
   private
 
   def set_token
     begin
-      @response.inspect
       if @response.code != 200
         p @response.message
+        close
       else
         @auth_token = @response["auth_token"]
+        open
       end
     rescue => e
+      close
       puts "Rescued #{e.inspect}"
     end
+  end
+
+  def open
+    @is_open = true
+    ["Session open", is_open]
+  end
+
+  def close
+    @is_open = false
+    ["Session closed", is_open]
   end
 
 end
--- lib/kele/user.rb
+++ lib/kele/user.rb
@@ -1,15 +1,31 @@
-# 
-# class User
-#
-#   def value(key)
-#     if @user
-#       @user["user"][key]
-#     else
-#       p "No active session."
-#     end
-#   end
-#
-#   def get_user
-#     @user = self.class.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => Session.auth_token })
-#   end
-# end
+require_relative "session"
+
+module User
+  extend self
+  include HTTParty
+
+  def load
+    if Session.open_status
+      @user = self.get("https://www.bloc.io/api/v1/users/me", headers: { "authorization" => Session.get_token })
+    else
+      p "No active session."
+    end
+  end
+
+  def response
+    @user
+  end
+
+  def get_value(key)
+    if @user
+      begin
+        @user[key]
+      rescue => e
+        p e.message
+      end
+    else
+      p "No user loaded."
+    end
+  end
+
+end
