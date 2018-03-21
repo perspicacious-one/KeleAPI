bfc37ac627b7b7ddb652eae7a61498d05d0974db
Nelson <nelsondcraig@gmail.com>
2018-03-21 15:17:29 -0700

    get thread(s) and send

--- lib/kele/mentor.rb
+++ lib/kele/mentor.rb
@@ -1,4 +1,5 @@
 require_relative "session"
+require_relative "helper"
 
 module Mentor
   extend self
@@ -9,12 +10,7 @@ module Mentor
   def get_availability(id)
     @id = id
     @response = self.get("https://www.bloc.io/api/v1/mentors/#{@id}/student_availability", headers: { "authorization" => Session.get_token })
-    if @response.code == 200
-      availability = []
-      @response.parsed_response.map { |hash| hash.each { |key, value| availability.push({key => value}) } }
-      availability
-    else
-      p @response.code + "\n" + @response.parsed_response
-    end
+    @response.parsed_response
   end
+
 end
